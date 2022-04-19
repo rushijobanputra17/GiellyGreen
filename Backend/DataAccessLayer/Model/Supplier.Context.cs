594fa72c5a7e1ec0c4224be1b99899f98ef1f9df
User32 <User32@PC-32>
2022-04-19 13:15:57 +0530

    Interfece and repository for invoice created

--- Backend/DataAccessLayer/Model/Supplier.Context.cs
+++ Backend/DataAccessLayer/Model/Supplier.Context.cs
@@ -123,5 +123,124 @@ namespace DataAccessLayer.Model
     
             return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<GetSupplier_Result>("GetSupplier", statusParameter);
         }
+    
+        public virtual ObjectResult<GetAllInvoice_Result> GetAllInvoice(Nullable<System.DateTime> invoiceMonth)
+        {
+            var invoiceMonthParameter = invoiceMonth.HasValue ?
+                new ObjectParameter("InvoiceMonth", invoiceMonth) :
+                new ObjectParameter("InvoiceMonth", typeof(System.DateTime));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<GetAllInvoice_Result>("GetAllInvoice", invoiceMonthParameter);
+        }
+    
+        public virtual ObjectResult<Nullable<int>> InsertInvoice(Nullable<int> invoiceId, string invoiceRef, Nullable<System.DateTime> invoiceDate, string customCol1, string customCol2, string customCol3, string customCol4, string customCol5)
+        {
+            var invoiceIdParameter = invoiceId.HasValue ?
+                new ObjectParameter("InvoiceId", invoiceId) :
+                new ObjectParameter("InvoiceId", typeof(int));
+    
+            var invoiceRefParameter = invoiceRef != null ?
+                new ObjectParameter("InvoiceRef", invoiceRef) :
+                new ObjectParameter("InvoiceRef", typeof(string));
+    
+            var invoiceDateParameter = invoiceDate.HasValue ?
+                new ObjectParameter("InvoiceDate", invoiceDate) :
+                new ObjectParameter("InvoiceDate", typeof(System.DateTime));
+    
+            var customCol1Parameter = customCol1 != null ?
+                new ObjectParameter("CustomCol1", customCol1) :
+                new ObjectParameter("CustomCol1", typeof(string));
+    
+            var customCol2Parameter = customCol2 != null ?
+                new ObjectParameter("CustomCol2", customCol2) :
+                new ObjectParameter("CustomCol2", typeof(string));
+    
+            var customCol3Parameter = customCol3 != null ?
+                new ObjectParameter("CustomCol3", customCol3) :
+                new ObjectParameter("CustomCol3", typeof(string));
+    
+            var customCol4Parameter = customCol4 != null ?
+                new ObjectParameter("CustomCol4", customCol4) :
+                new ObjectParameter("CustomCol4", typeof(string));
+    
+            var customCol5Parameter = customCol5 != null ?
+                new ObjectParameter("CustomCol5", customCol5) :
+                new ObjectParameter("CustomCol5", typeof(string));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<Nullable<int>>("InsertInvoice", invoiceIdParameter, invoiceRefParameter, invoiceDateParameter, customCol1Parameter, customCol2Parameter, customCol3Parameter, customCol4Parameter, customCol5Parameter);
+        }
+    
+        public virtual int InsertUpdateInvoiceDetails(Nullable<int> invoiceDetailId, Nullable<int> supplierId, string supplierName, Nullable<decimal> hairServices, Nullable<decimal> beautyServices, Nullable<decimal> customService1, Nullable<decimal> customService2, Nullable<decimal> customService3, Nullable<decimal> customService4, Nullable<decimal> customService5, Nullable<decimal> net, Nullable<decimal> vAT, Nullable<decimal> gross, Nullable<decimal> advancePaid, Nullable<decimal> balanceDue, Nullable<bool> approved, Nullable<int> invoiceId)
+        {
+            var invoiceDetailIdParameter = invoiceDetailId.HasValue ?
+                new ObjectParameter("InvoiceDetailId", invoiceDetailId) :
+                new ObjectParameter("InvoiceDetailId", typeof(int));
+    
+            var supplierIdParameter = supplierId.HasValue ?
+                new ObjectParameter("SupplierId", supplierId) :
+                new ObjectParameter("SupplierId", typeof(int));
+    
+            var supplierNameParameter = supplierName != null ?
+                new ObjectParameter("SupplierName", supplierName) :
+                new ObjectParameter("SupplierName", typeof(string));
+    
+            var hairServicesParameter = hairServices.HasValue ?
+                new ObjectParameter("HairServices", hairServices) :
+                new ObjectParameter("HairServices", typeof(decimal));
+    
+            var beautyServicesParameter = beautyServices.HasValue ?
+                new ObjectParameter("BeautyServices", beautyServices) :
+                new ObjectParameter("BeautyServices", typeof(decimal));
+    
+            var customService1Parameter = customService1.HasValue ?
+                new ObjectParameter("CustomService1", customService1) :
+                new ObjectParameter("CustomService1", typeof(decimal));
+    
+            var customService2Parameter = customService2.HasValue ?
+                new ObjectParameter("CustomService2", customService2) :
+                new ObjectParameter("CustomService2", typeof(decimal));
+    
+            var customService3Parameter = customService3.HasValue ?
+                new ObjectParameter("CustomService3", customService3) :
+                new ObjectParameter("CustomService3", typeof(decimal));
+    
+            var customService4Parameter = customService4.HasValue ?
+                new ObjectParameter("CustomService4", customService4) :
+                new ObjectParameter("CustomService4", typeof(decimal));
+    
+            var customService5Parameter = customService5.HasValue ?
+                new ObjectParameter("CustomService5", customService5) :
+                new ObjectParameter("CustomService5", typeof(decimal));
+    
+            var netParameter = net.HasValue ?
+                new ObjectParameter("Net", net) :
+                new ObjectParameter("Net", typeof(decimal));
+    
+            var vATParameter = vAT.HasValue ?
+                new ObjectParameter("VAT", vAT) :
+                new ObjectParameter("VAT", typeof(decimal));
+    
+            var grossParameter = gross.HasValue ?
+                new ObjectParameter("Gross", gross) :
+                new ObjectParameter("Gross", typeof(decimal));
+    
+            var advancePaidParameter = advancePaid.HasValue ?
+                new ObjectParameter("AdvancePaid", advancePaid) :
+                new ObjectParameter("AdvancePaid", typeof(decimal));
+    
+            var balanceDueParameter = balanceDue.HasValue ?
+                new ObjectParameter("BalanceDue", balanceDue) :
+                new ObjectParameter("BalanceDue", typeof(decimal));
+    
+            var approvedParameter = approved.HasValue ?
+                new ObjectParameter("Approved", approved) :
+                new ObjectParameter("Approved", typeof(bool));
+    
+            var invoiceIdParameter = invoiceId.HasValue ?
+                new ObjectParameter("InvoiceId", invoiceId) :
+                new ObjectParameter("InvoiceId", typeof(int));
+    
+            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction("InsertUpdateInvoiceDetails", invoiceDetailIdParameter, supplierIdParameter, supplierNameParameter, hairServicesParameter, beautyServicesParameter, customService1Parameter, customService2Parameter, customService3Parameter, customService4Parameter, customService5Parameter, netParameter, vATParameter, grossParameter, advancePaidParameter, balanceDueParameter, approvedParameter, invoiceIdParameter);
+        }
     }
 }
---
+++ Backend/DataAccessLayer/Services/MonthlyInvoiceRepository.cs
@@ -0,0 +1,26 @@
+﻿using DataAccessLayer.Interface;
+using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Services
+{
+    internal class MonthlyInvoiceRepository : IMonthlyInvoiceRepository
+    {
+        private Team2_GiellyGreenEntities objDataAccess;
+
+        public MonthlyInvoiceRepository()
+        {
+            objDataAccess = new Team2_GiellyGreenEntities();
+        }
+
+        public List<object> GetSuppliers(DateTime InvoiceMonth)
+        {
+            return objDataAccess.GetAllInvoice(InvoiceMonth);
+        }
+
+    }
+}
