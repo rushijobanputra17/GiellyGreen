2f64e6e357e19ca55484ff932d92d90f1096b1a8
User32 <User32@PC-32>
2022-04-22 11:56:28 +0530

    pdf view update

--- Backend/GiellyGreen/App_Start/BundleConfig.cs
+++ Backend/GiellyGreen/App_Start/BundleConfig.cs
@@ -17,10 +17,11 @@ namespace GiellyGreen
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js","~/Scripts/bootstrap.min.js","~/Scripts/jquery-3.4.1.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
