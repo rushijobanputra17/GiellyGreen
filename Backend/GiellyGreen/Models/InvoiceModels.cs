376d5ee50f4c457f1af15e69491c16dfb5f191a5
Harsh-Mungra <harshmungra18112000@gmail.com>
2022-04-26 15:17:11 +0530

    InvoiceRef field set to Required

--- Backend/GiellyGreen/Models/InvoiceModels.cs
+++ Backend/GiellyGreen/Models/InvoiceModels.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace GiellyGreen.Models
 {
@@ -10,6 +11,8 @@ namespace GiellyGreen.Models
         public string CustomHeader3 { get; set; }
         public string CustomHeader4 { get; set; }
         public string CustomHeader5 { get; set; }
+
+        [Required]
         public string InvoiceRef { get; set; }
         public Nullable<System.DateTime> InvoiceDate { get; set; }
         public int InvoiceId { get; set; }
